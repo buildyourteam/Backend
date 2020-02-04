f054a76ed29da2ab2ed4103b796022c0ceffd739
김도영 <rlaehdud4992@ssu.ac.kr>
2020-02-04 21:00:59 +0900

    MemberController, MemberService 작성 1. MemberController url 설정 완료 2. MemberController 기능 구현을 위한 MemberService 일부 작성
    @TODO
    MemberService를 MemberController에 적용시키기 위해 TDD를 적용하여
    개발하기

--- src/main/java/com/eskiiimo/api/people/MemberService.java
+++ src/main/java/com/eskiiimo/api/people/MemberService.java
@@ -3,12 +3,33 @@ package com.eskiiimo.api.people;
 import com.eskiiimo.api.people.dto.MemberDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class MemberService {
     private final MemberRepository memberRepository;
 
-//    Long save(MemberDto memberDto) {
-//    }
+    @Transactional
+    Long save(Member member) {
+        return memberRepository.save(member).getMemberId();
+    }
+
+    @Transactional(readOnly = true)
+    Member getOne(Long user_id) {
+        return memberRepository.getOne(user_id);
+    }
+
+    @Transactional(readOnly = true)
+    List<Member> getMemberList() {
+        return memberRepository.findAll();
+    }
+
+    Long deleteMember(Long member_id) {
+        memberRepository.deleteById(member_id);
+        return member_id;
+    }
+
 }
