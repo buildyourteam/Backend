27cc96a674328b65b5b0dc7a6e9432f8b212197e
황상일 <si8363@soongsil.ac.kr>
2020-03-18 01:14:30 +0900

    #수정사항 ## 클라이언트 접속시 CORS error 발생으로 인한 세팅 변경 - WebMvcConfig - SecurityConfiguration ## 클라이언트에서 토큰전송시 대문자 이슈가 발생해 토큰명 소문자로 변경 - AuthController - WebMvcConfig - SecurityConfiguration

--- src/main/java/com/eskiiimo/api/configs/WebMvcConfig.java
+++ src/main/java/com/eskiiimo/api/configs/WebMvcConfig.java
@@ -10,8 +10,9 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("*")
-                .exposedHeaders("authToken")	//make client read header("jwt-token")
-        ;
+                .exposedHeaders("authtoken")	//make client read header("jwt-token")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS",  "HEAD", "TRACE", "CONNECT")
+                .allowCredentials(true);
 
     }
 }
