cf03b87e0301261cbea7e6ab8b90783a226a56b5
always0ne <si8363@naver.com>
2020-07-12 15:35:28 +0900

    refactor: people HAL 제거
    ##PeopleController
    - Controller-> RestController
    
    ## PeopletResource 제거
    
    Resolves: #88
    See also: #82

--- src/main/java/com/eskiiimo/web/user/controller/PeopleController.java
+++ src/main/java/com/eskiiimo/web/user/controller/PeopleController.java
@@ -1,30 +1,21 @@
 package com.eskiiimo.web.user.controller;
 
 import com.eskiiimo.repository.user.model.People;
-import com.eskiiimo.web.index.controller.DocsController;
 import com.eskiiimo.web.projects.enumtype.ProjectRole;
-import com.eskiiimo.web.user.controller.resource.PeopleResource;
 import com.eskiiimo.web.user.service.PeopleService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PagedResourcesAssembler;
+import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.PagedModel;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import static org.springframework.hateoas.server.mvc.ControllerLinkBuilder.linkTo;
+import org.springframework.web.bind.annotation.*;
 
 
-@Controller
+@RestController
 @CrossOrigin(origins = "*")
 @RequiredArgsConstructor
-@RequestMapping(value ="/people")
+@RequestMapping(value = "/people")
 public class PeopleController {
 
     @Autowired
@@ -32,16 +23,13 @@ public class PeopleController {
 
 
     @GetMapping
-    public ResponseEntity getJobSeekers(Pageable pageable, PagedResourcesAssembler<People> assembler,
-                                        @RequestParam(value = "grade", required = false)Long grade,
-                                        @RequestParam(value = "role",required =  false) ProjectRole role,
-                                        @RequestParam(value = "area",required = false)String area
+    public PagedModel<EntityModel<People>> getJobSeekers(
+            Pageable pageable,
+            PagedResourcesAssembler<People> assembler,
+            @RequestParam(value = "grade", required = false) Long grade,
+            @RequestParam(value = "role", required = false) ProjectRole role,
+            @RequestParam(value = "area", required = false) String area
     ) {
-        Page<People> page = peopleService.getPeople(grade,role,area,pageable);
-        PagedModel<PeopleResource> pagedResources = assembler.toModel(page, e -> new PeopleResource(e));
-        pagedResources.add(linkTo(DocsController.class).slash("resourcesPeople").withRel("profile"));
-
-        return ResponseEntity.ok(pagedResources);
-
+        return assembler.toModel(peopleService.getPeople(grade, role, area, pageable));
     }
 }
