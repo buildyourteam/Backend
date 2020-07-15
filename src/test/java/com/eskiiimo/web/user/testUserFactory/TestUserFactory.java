fd0376f75d84a16ae431234dd6f852a95dc7ab3d
DESKTOP-김진영\rlawl <rlawlsdud419@naver.com>
2020-07-15 18:32:58 +0900

    test: refactoring PeopleControllerTest 현재 중복되어 있는 Code Refactoring
    ## PeopleControllerTest
    PeopleControllerTest클래스 내의 중복 코드 리팩토링 진행
    
    ## BaseControllerTest
    TestUserFactory를 DI받음
    
    ## TestUserFactory
    중복되어 있는 메소드를 따로 제작한 클래스
    
    Resolves: #41
    See also:

---
+++ src/test/java/com/eskiiimo/web/user/testUserFactory/TestUserFactory.java
@@ -0,0 +1,53 @@
+package com.eskiiimo.web.user.testUserFactory;
+
+import com.eskiiimo.repository.user.model.User;
+import com.eskiiimo.repository.user.model.UsersStack;
+import com.eskiiimo.repository.user.repository.UserRepository;
+import com.eskiiimo.web.projects.enumtype.ProjectRole;
+import com.eskiiimo.web.projects.enumtype.TechnicalStack;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
+
+@Configuration
+public class TestUserFactory {
+
+    @Autowired
+    UserRepository userRepository;
+
+    public User generateUser(int index){
+        List<UsersStack> stacks1 = new ArrayList<UsersStack>();
+        stacks1.add(new UsersStack(TechnicalStack.SPRINGBOOT));
+        User user =  User.builder()
+                .userId("testUser"+(3*index+1))
+                .password("testpassword")
+                .grade((long)1)
+                .stacks(stacks1)
+                .area("Seoul")
+                .userName("User"+(3*index+1))
+                .role(ProjectRole.DEVELOPER)
+                .build();
+        return this.userRepository.save(user);
+    }
+
+    public User generateLeader(int index){
+        List<UsersStack> stacks1 = new ArrayList<UsersStack>();
+        stacks1.add(new UsersStack(TechnicalStack.SPRINGBOOT));
+        User user =  User.builder()
+                .userId("testLeader"+(3*index+1))
+                .password("testpassword")
+                .grade((long)1)
+                .stacks(stacks1)
+                .area("Seoul")
+                .userName("User"+(3*index+1))
+                .role(ProjectRole.LEADER)
+                .build();
+        return this.userRepository.save(user);
+    }
+
+    public void generatePeople() {
+        IntStream.range(0,4).forEach(this::generateUser);
+    }
+}
