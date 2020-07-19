3abe8c08180d6f07937ea70b21e86634791ba8ef
DESKTOP-김진영\rlawl <rlawlsdud419@naver.com>
2020-07-19 21:06:25 +0900

    test: refactoring ProjectListControllerTest
    ## ProjectListControllerTests
     1) generateProjects()메소드를 따로 작성하여 중복코드 해결
     2) 필요없는 테스트/메소드 제거
     - 마감임박한 프로젝트 리스트 조회 테스트
     - generateProjectDeadline() 메소드
    
    ## TestProjectFactory
     1) generateProject(..) : 메소드 오버로딩 -> 가독성 증가
    
    Resolves: #92
    See also:  #41

--- src/test/java/com/eskiiimo/web/projects/controller/ProjectListControllerTests.java
+++ src/test/java/com/eskiiimo/web/projects/controller/ProjectListControllerTests.java
@@ -1,20 +1,11 @@
 package com.eskiiimo.web.projects.controller;
 
-import com.eskiiimo.repository.projects.model.Project;
-import com.eskiiimo.repository.projects.repository.ProjectMemberRepository;
-import com.eskiiimo.repository.projects.repository.ProjectRepository;
-import com.eskiiimo.repository.user.repository.UserRepository;
 import com.eskiiimo.web.common.BaseControllerTest;
 import com.eskiiimo.web.projects.enumtype.ProjectField;
-import com.eskiiimo.web.projects.enumtype.ProjectMemberSet;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.time.LocalDateTime;
-import java.util.stream.IntStream;
 
 import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
 import static org.springframework.restdocs.headers.HeaderDocumentation.responseHeaders;
@@ -33,28 +24,16 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @DisplayName("프로젝트 리스트")
 class ProjectListControllerTests extends BaseControllerTest {
 
-    @Autowired
-    ProjectRepository projectRepository;
-
-    @Autowired
-    ProjectMemberRepository projectMemberRepository;
-
-    @Autowired
-    UserRepository userRepository;
-
-
     @Test
     @Transactional
     @DisplayName("검색기능사용 없이 전체리스트 조회")
     void queryProjectsTotal() throws Exception {
         // Given
-        IntStream.range(0, 30).forEach(i -> {
-            this.generateEvent(i);
-        });
+        generateProjects();
 
         // When & Then
         this.mockMvc.perform(get("/projects")
-                .param("page", "1")
+                .param("page", "0")
                 .param("size", "10")
                 .param("sort", "projectName,DESC")
         )
@@ -72,8 +51,7 @@ class ProjectListControllerTests extends BaseControllerTest {
     @DisplayName("직군별 and 분야별 프로젝트 리스트 조회하기")
     void queryProjectsOccupationAndField() throws Exception {
         // Given
-        this.generateEvent(0);
-        this.generateEvent(1);
+        generateProjects();
 
         // When & Then
         this.mockMvc.perform(get("/projects")
@@ -136,8 +114,7 @@ class ProjectListControllerTests extends BaseControllerTest {
     @DisplayName("only 직군별 프로젝트 리스트 조회하기")
     void queryProjectsOccupation() throws Exception {
         // Given
-        this.generateEvent(0);
-        this.generateEvent(1);
+        generateProjects();
 
         // When & Then
         this.mockMvc.perform(get("/projects")
@@ -159,8 +136,7 @@ class ProjectListControllerTests extends BaseControllerTest {
     @DisplayName("only 분야별로만 프로젝트 리스트 조회하기")
     void queryProjectsField() throws Exception {
         // Given
-        this.generateEvent(0);
-        this.generateEvent(1);
+        generateProjects();
 
         // When & Then
         this.mockMvc.perform(get("/projects")
@@ -177,144 +153,14 @@ class ProjectListControllerTests extends BaseControllerTest {
 
     }
 
-    @Test
     @Transactional
-    @DisplayName("마감임박한 프로젝트 리스트 조회")
-    void DeadlineProjectList() throws Exception {
-        // Given
-        this.generateProjectDeadline(0);
-
-        // When & Then
-        this.mockMvc.perform(get("/projects/deadline")
-                .param("page", "0")
-                .param("size", "10")
-                .param("sort", "projectName,DESC")
-        )
-                .andDo(print())
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("page").exists())
-                .andDo(document("get-deadline-project",
-                        links(
-                                linkWithRel("self").description("link to self")
-                        ),
-                        requestParameters(
-                                parameterWithName("page").description("page"),
-                                parameterWithName("size").description("number of projects per page"),
-                                parameterWithName("sort").description("sort")
-                        ),
-                        responseHeaders(
-                                headerWithName(HttpHeaders.CONTENT_TYPE).description("content type header")
-                        ),
-                        responseFields(
-
-                                fieldWithPath("_embedded.projectList[].projectId").description("프로젝트 아이디 (=이미지 파일 이름)"),
-                                fieldWithPath("_embedded.projectList[].projectName").description("프로젝트 이름"),
-                                fieldWithPath("_embedded.projectList[].teamName").description("팀명"),
-                                fieldWithPath("_embedded.projectList[].endDate").description("마감일"),
-                                fieldWithPath("_embedded.projectList[].introduction").description("프로젝트에 대한 설명"),
-                                fieldWithPath("_embedded.projectList[].state").description("프로젝트 상태(모집중, 진행중, 마감)"),
-                                fieldWithPath("_embedded.projectList[].projectField").description("프로젝트 분야(앱, 웹, AI 등등.."),
-                                fieldWithPath("_embedded.projectList[].leaderId").description("팀장 아이디"),
-                                fieldWithPath("_embedded.projectList[].currentMember.developer").description("현재 개발자 수"),
-                                fieldWithPath("_embedded.projectList[].currentMember.designer").description("현재 디자이너 수"),
-                                fieldWithPath("_embedded.projectList[].currentMember.planner").description("현재 기획자 수"),
-                                fieldWithPath("_embedded.projectList[].currentMember.etc").description("현재 기타 수"),
-                                fieldWithPath("_embedded.projectList[].needMember.developer").description("필요한 개발자 수"),
-                                fieldWithPath("_embedded.projectList[].needMember.designer").description("필요한 디자이너 수"),
-                                fieldWithPath("_embedded.projectList[].needMember.planner").description("필요한 기획자 수"),
-                                fieldWithPath("_embedded.projectList[].needMember.etc").description("그 외 필요한 인원 수"),
-                                fieldWithPath("_links.self.href").description("self 링크"),
-                                fieldWithPath("page.size").description("한 페이지 당 프로젝트 갯수"),
-                                fieldWithPath("page.totalElements").description("총 프로젝트 갯수"),
-                                fieldWithPath("page.totalPages").description("총 페이지 수"),
-                                fieldWithPath("page.number").description("페이지 수")
-
-                        )
-                ))
-        ;
+    void generateProjects() {
+        this.testProjectFactory.generateProject(0, ProjectField.AI, false);
+        this.testProjectFactory.generateProject(1, ProjectField.APP, true);
+        this.testProjectFactory.generateProject(2, ProjectField.WEB, true);
+        this.testProjectFactory.generateProject(3, ProjectField.WEB, true);
 
     }
 
-
-    private void generateEvent(int index) {
-
-        ProjectMemberSet need_zero = new ProjectMemberSet(0, 2, 3, 4);
-        ProjectMemberSet need_yes = new ProjectMemberSet(1, 4, 6, 8);
-        ProjectMemberSet currentMember = new ProjectMemberSet(2, 1, 1, 2);
-
-        Project project = Project.builder()
-                .projectName("project" + index)
-                .teamName("project team" + index * 2)
-                .endDate(LocalDateTime.of(2020, 04, 30, 23, 59))
-                .introduction("need yes 입니다.")
-                .currentMember(currentMember)
-                .needMember(need_yes)
-                .projectField(ProjectField.APP)
-                .build();
-
-        Project project1 = Project.builder()
-                .projectName("project" + index)
-                .teamName("project team" + index * 2)
-                .endDate(LocalDateTime.of(2020, 04, 30, 23, 59))
-                .introduction("need zero 입니다.")
-                .currentMember(currentMember)
-                .needMember(need_zero)
-                .projectField(ProjectField.WEB)
-                .build();
-
-        Project project2 = Project.builder()
-                .projectName("project" + index)
-                .teamName("project team" + index * 2)
-                .endDate(LocalDateTime.of(2020, 04, 30, 23, 59))
-                .introduction("need yes 입니다.")
-                .currentMember(currentMember)
-                .needMember(need_yes)
-                .projectField(ProjectField.WEB)
-                .build();
-
-        this.projectRepository.save(project);
-        this.projectRepository.save(project1);
-        this.projectRepository.save(project2);
-
-    }
-
-    private void generateProjectDeadline(int index) {
-
-        ProjectMemberSet need_zero = new ProjectMemberSet(0, 2, 3, 4);
-        ProjectMemberSet need_yes = new ProjectMemberSet(1, 4, 6, 8);
-        ProjectMemberSet currentMember = new ProjectMemberSet(2, 1, 1, 2);
-
-        Project project = Project.builder()
-                .projectName("project" + index)
-                .teamName("project team" + index * 2)
-                .endDate(LocalDateTime.of(2020, 2, 28, 23, 59))
-                .introduction("need yes 입니다.")
-                .currentMember(currentMember)
-                .needMember(need_yes)
-                .build();
-
-        Project project1 = Project.builder()
-                .projectName("project" + index)
-                .teamName("project team" + index * 2)
-                .endDate(LocalDateTime.of(2020, 2, 14, 23, 59))
-                .introduction("need zero 입니다.")
-                .currentMember(currentMember)
-                .needMember(need_zero)
-                .build();
-
-        Project project2 = Project.builder()
-                .projectName("project" + index)
-                .teamName("project team" + index * 2)
-                .endDate(LocalDateTime.of(2020, 03, 30, 23, 59))
-                .introduction("need zero 입니다.")
-                .currentMember(currentMember)
-                .needMember(need_zero)
-                .build();
-
-        this.projectRepository.save(project);
-        this.projectRepository.save(project1);
-        this.projectRepository.save(project2);
-
-    }
 
 }
--- src/test/java/com/eskiiimo/web/projects/testProjectFactory/TestProjectFactory.java
+++ src/test/java/com/eskiiimo/web/projects/testProjectFactory/TestProjectFactory.java
@@ -9,7 +9,6 @@ import com.eskiiimo.repository.user.model.User;
 import com.eskiiimo.web.projects.enumtype.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -35,9 +34,13 @@ public class TestProjectFactory {
                 .userName("회원 01")
                 .grade((long) 100)
                 .build();
+
         return profileDto;
     }
 
+    /*
+    프로젝트를 생성하고, 해당 프로젝트 팀장을 연결
+     */
     public Project generateProject(int index, User user, State status) {
         Project project = Project.builder()
                 .projectName("project" + index)
@@ -58,11 +61,43 @@ public class TestProjectFactory {
                 .introduction("프로젝트 팀장 입니다.")
                 .build();
         project.getProjectMembers().add(projectMember);
+
         this.projectMemberRepository.save(projectMember);
         this.projectRepository.save(project);
         return project;
     }
 
+    /*
+    프로젝트 필터(분야,직군)별 검색기능 테스트를 위한 프로젝트 생성
+    */
+    public Project generateProject(int index, ProjectField projectField, Boolean is_need) {
+
+        ProjectMemberSet need_zero = new ProjectMemberSet(0, 0, 0, 0);
+        ProjectMemberSet need_yes = new ProjectMemberSet(1, 4, 6, 8);
+        ProjectMemberSet needMember;
+        if(is_need.equals(true)) {
+            needMember = need_yes;
+        }
+        else {
+            needMember = need_zero;
+        }
+        Project project = Project.builder()
+                .projectName("project" + index)
+                .teamName("project team" + index * 2)
+                .endDate(LocalDateTime.of(2022, 04, 30, 23, 59))
+                .introduction("need yes 입니다.")
+                .currentMember(new ProjectMemberSet(2, 1, 1, 2))
+                .needMember(needMember)
+                .projectField(projectField)
+                .build();
+
+        this.projectRepository.save(project);
+        return project;
+    }
+
+    /*
+    프로젝트 팀원 매칭
+     */
     public ProjectMember generateProjectMember(User user, Project project, Boolean hide) {
         ProjectMember projectMember = ProjectMember.builder()
                 .project(project)
@@ -72,6 +107,7 @@ public class TestProjectFactory {
                 .hide(hide)
                 .build();
         project.getProjectMembers().add(projectMember);
+
         this.projectMemberRepository.save(projectMember);
         this.projectRepository.save(project);
         return projectMember;
