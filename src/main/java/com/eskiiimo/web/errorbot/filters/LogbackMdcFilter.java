53d5f08488ff45a4a261cd3da195504fa19baf35
always0ne <si8363@naver.com>
2020-07-24 01:32:25 +0900

    refactor: 테스트때문에 사용하지 못했던  errorBot의 UserAgent 수집 기능 재활성화
    ### UserAgent 헤더가 존재하지 않을 시
    - UNKNOWN처리
    
    ### 문제점
    - PostMan의 UserAgent를 제대로 파싱하지 못해 에러봇이 작동을 못함
    
    **PostMan의 UserAgent가 이상한것**
    
    PostMan으로 테스트를 할 때
    **Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36**
    와 같이 UserAgent를 수정하여 요청을 보내거나 UserAgent 헤더를 삭제할 것
    
    Resolves: #117
    See also: -

--- src/main/java/com/eskiiimo/web/errorbot/filters/LogbackMdcFilter.java
+++ src/main/java/com/eskiiimo/web/errorbot/filters/LogbackMdcFilter.java
@@ -1,11 +1,13 @@
 package com.eskiiimo.web.errorbot.filters;
 
+import com.eskiiimo.web.errorbot.util.AgentUtils;
 import com.eskiiimo.web.errorbot.util.HttpUtils;
 import com.eskiiimo.web.errorbot.util.MDCUtil;
 import com.eskiiimo.web.errorbot.util.RequestWrapper;
 import org.slf4j.MDC;
 
 import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 
 public class LogbackMdcFilter implements Filter {
@@ -32,7 +34,7 @@ public class LogbackMdcFilter implements Filter {
         MDCUtil.setJsonValue(MDCUtil.USER_INFO_MDC, HttpUtils.getCurrentUser());
 
         // Set Agent Detail
-       // MDCUtil.setJsonValue(MDCUtil.AGENT_DETAIL_MDC, AgentUtils.getAgentDetail((HttpServletRequest) request));
+        MDCUtil.setJsonValue(MDCUtil.AGENT_DETAIL_MDC, AgentUtils.getAgentDetail((HttpServletRequest) request));
 
         // Set Http Request URI
         MDCUtil.set(MDCUtil.REQUEST_URI_MDC, requestWrapper.getRequestUri());
