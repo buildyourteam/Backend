ee8f7755b0240c8c1cf9435bf4339c06812935d7
always0ne <si8363@naver.com>
2020-07-27 21:35:46 +0900

    refactor: 클라이언트용 test데이터는 빌드테스트에서 생성되면 안된다.
    - @Profile(!test)를 사용하여 테스트에서 빈 생성 금지
    - ContextLoad 테스트에서 클라이언트용 test데이터 생성이 실행되어 무시처리

--- src/main/java/com/eskiiimo/web/configs/InitTestData.java
+++ src/main/java/com/eskiiimo/web/configs/InitTestData.java
@@ -14,6 +14,7 @@ import com.eskiiimo.web.user.service.ProfileService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.context.event.ApplicationStartedEvent;
 import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
@@ -21,6 +22,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Component
+@Profile("!test")
 @RequiredArgsConstructor
 public class InitTestData implements ApplicationListener<ApplicationStartedEvent> {
 
