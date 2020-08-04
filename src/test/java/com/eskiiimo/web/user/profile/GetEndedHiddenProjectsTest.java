beab12008ee305106d2fb895463e6a5eecaba15b
DESKTOP-김진영\rlawl <rlawlsdud419@naver.com>
2020-08-04 20:03:58 +0900

    test: ProfileControllerTest 메소드마다 파일 분리 ## ProfileControllerTest 1) GetEndedHiddenProjectsTest   - 참여했던 숨겨진 프로젝트   - 권한 없는 사용자일 때   - 프로젝트가 존재하지 않을 때 2) GetEndedProjectsTest   - 참여했던 프로젝트   - 프로젝트가 존재하지 않을 때 3) GetMyPlanHiddenProjectsTest   - 기획한 숨겨진 프로젝트   - 권한 없는 사용자일 때   - 프로젝트가 존재하지 않을 때 4) GetMyPlanProjectsTest   - 기획한 프로젝트   - 프로젝트가 존재하지 않을 때 5) GetProfileTest   - 본인의 프로필 조회   - 사용자 프로필 조회 6) GetRunningHiddenProjectsTest   - 참여중인 숨겨진 프로젝트   - 권한 없는 사용자일 때   - 프로젝트가 존재하지 않을 때 7) GetRunningProjectsTest   - 참여중인 숨겨진 프로젝트   - 프로젝트가 존재하지 않을 때 8) HideProjectTest   - 프로젝트 숨기기   - 권한없는 사용자일 때 9) ReShowProjectTest   - 프로젝트 다시 보이기   - 권한 없는 사용자일 때 10) UpdateProfileTest   - 프로필 수정   - 권한 없는 사용자일 때
    Resolves: #66
    See also: -

---
+++ src/test/java/com/eskiiimo/web/user/profile/GetEndedHiddenProjectsTest.java
@@ -0,0 +1,140 @@
+package com.eskiiimo.web.user.profile;
+
+import com.eskiiimo.repository.projects.model.Project;
+import com.eskiiimo.repository.user.model.User;
+import com.eskiiimo.web.common.BaseControllerTest;
+import com.eskiiimo.web.projects.enumtype.State;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
+import static org.springframework.restdocs.headers.HeaderDocumentation.responseHeaders;
+import static org.springframework.restdocs.hypermedia.HypermediaDocumentation.linkWithRel;
+import static org.springframework.restdocs.hypermedia.HypermediaDocumentation.links;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.requestParameters;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@DisplayName("참여했던 숨겨진 프로젝트")
+public class GetEndedHiddenProjectsTest extends BaseControllerTest {
+
+    @Test
+    @Transactional
+    @WithMockUser(username = "user1")
+    @DisplayName("사용자가 참여했던 숨겨진 프로젝트 리스트 가져오기")
+    public void getEndedHiddenProjectList() throws Exception {
+        // Given
+        User user1 = testUserFactory.generateUser(1);
+        User user2 = testUserFactory.generateUser(2);
+        Project project1 = testProjectFactory.generateProject(1, user2, State.ENDED);
+        Project project4 = testProjectFactory.generateProject(4, user2, State.ENDED);
+        testProjectFactory.generateProjectMember(user1, project4, Boolean.TRUE);
+        testProjectFactory.generateProjectMember(user1, project1, Boolean.FALSE);
+
+        // When & Then
+        this.mockMvc.perform(get("/profile/user1/ended/hidden")
+                .param("page", "0")
+                .param("size", "10")
+                .param("sort", "projectName,DESC")
+        )
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andDo(document("get-ended-hidden-project",
+                        links(
+                                linkWithRel("self").description("link to self")
+                        ),
+                        requestParameters(
+                                parameterWithName("page").description("page"),
+                                parameterWithName("size").description("number of projects per page"),
+                                parameterWithName("sort").description("sort")
+                        ),
+                        responseHeaders(
+                                headerWithName(HttpHeaders.CONTENT_TYPE).description("content type header")
+                        ),
+                        responseFields(
+
+                                fieldWithPath("_embedded.projectList[].projectId").description("프로젝트 아이디 (=이미지 파일 이름)"),
+                                fieldWithPath("_embedded.projectList[].projectName").description("프로젝트 이름"),
+                                fieldWithPath("_embedded.projectList[].teamName").description("팀명"),
+                                fieldWithPath("_embedded.projectList[].endDate").description("마감일"),
+                                fieldWithPath("_embedded.projectList[].introduction").description("프로젝트에 대한 설명"),
+                                fieldWithPath("_embedded.projectList[].state").description("프로젝트 상태(모집중, 진행중, 마감)"),
+                                fieldWithPath("_embedded.projectList[].projectField").description("프로젝트 분야(앱, 웹, AI 등등.."),
+                                fieldWithPath("_embedded.projectList[].leaderId").description("팀장 아이디"),
+                                fieldWithPath("_embedded.projectList[].currentMember.developer").description("현재 개발자 수"),
+                                fieldWithPath("_embedded.projectList[].currentMember.designer").description("현재 디자이너 수"),
+                                fieldWithPath("_embedded.projectList[].currentMember.planner").description("현재 기획자 수"),
+                                fieldWithPath("_embedded.projectList[].currentMember.etc").description("현재 기타 수"),
+                                fieldWithPath("_embedded.projectList[].needMember.developer").description("필요한 개발자 수"),
+                                fieldWithPath("_embedded.projectList[].needMember.designer").description("필요한 디자이너 수"),
+                                fieldWithPath("_embedded.projectList[].needMember.planner").description("필요한 기획자 수"),
+                                fieldWithPath("_embedded.projectList[].needMember.etc").description("그 외 필요한 인원 수"),
+                                fieldWithPath("_links.self.href").description("self 링크"),
+                                fieldWithPath("page.size").description("한 페이지 당 프로젝트 갯수"),
+                                fieldWithPath("page.totalElements").description("총 프로젝트 갯수"),
+                                fieldWithPath("page.totalPages").description("총 페이지 수"),
+                                fieldWithPath("page.number").description("페이지 수")
+
+                        )
+                ))
+
+        ;
+
+    }
+
+    @Test
+    @Transactional
+    @WithMockUser(username = "user0")
+    @DisplayName("사용자가 참여했던 숨겨진 프로젝트 리스트 가져오기_권한없는 사용자")
+    public void getEndedHiddenProjectList_AuthX() throws Exception {
+        // Given
+        User user1 = testUserFactory.generateUser(1);
+        User user2 = testUserFactory.generateUser(2);
+        Project project1 = testProjectFactory.generateProject(1, user2, State.ENDED);
+        Project project4 = testProjectFactory.generateProject(4, user2, State.ENDED);
+        testProjectFactory.generateProjectMember(user1, project4, Boolean.TRUE);
+        testProjectFactory.generateProjectMember(user1, project1, Boolean.FALSE);
+
+        // When & Then
+        this.mockMvc.perform(get("/profile/user1/ended/hidden")
+                .param("page", "0")
+                .param("size", "10")
+                .param("sort", "projectName,DESC")
+        )
+                .andDo(print())
+                .andExpect(status().isForbidden())
+        ;
+    }
+
+    @Test
+    @Transactional
+    @WithMockUser(username = "user1")
+    @DisplayName("사용자가 참여했던 숨겨진 프로젝트 리스트 가져오기_프로젝트가 없을 때")
+    public void getEndedHiddenProjectList_notExist() throws Exception {
+        // Given
+        User user1 = testUserFactory.generateUser(1);
+        User user2 = testUserFactory.generateUser(2);
+        Project project1 = testProjectFactory.generateProject(1, user2, State.ENDED);
+        Project project4 = testProjectFactory.generateProject(4, user2, State.ENDED);
+        testProjectFactory.generateProjectMember(user1, project4, Boolean.FALSE);
+        testProjectFactory.generateProjectMember(user1, project1, Boolean.FALSE);
+
+        // When & Then
+        this.mockMvc.perform(get("/profile/user1/ended/hidden")
+                .param("page", "0")
+                .param("size", "10")
+                .param("sort", "projectName,DESC")
+        )
+                .andDo(print())
+                .andExpect(status().isOk())
+        ;
+    }
+}
