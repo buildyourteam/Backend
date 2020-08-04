5861f9b5d5e88a6a44267efe0d19968f7397dac6
DESKTOP-김진영\rlawl <rlawlsdud419@naver.com>
2020-08-04 19:53:06 +0900

    refactor: change NotYourProfileException message  '프로필 수정권한이 없습니다' -> '당신의 프로필이 아닙니다' 로 메세지 변경
     Resolves: -
     See also: -

--- src/main/java/com/eskiiimo/web/user/exception/NotYourProfileException.java
+++ src/main/java/com/eskiiimo/web/user/exception/NotYourProfileException.java
@@ -2,6 +2,6 @@ package com.eskiiimo.web.user.exception;
 
 public class NotYourProfileException extends RuntimeException {
     public NotYourProfileException(String userId) {
-        super("userId : '" + userId + "' 프로필 수정 권한이 없습니다.");
+        super("userId : '" + userId + "' 당신의 프로필이 아닙니다.");
     }
 }
