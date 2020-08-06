1cde03a66f0f0ede5e1ace059515880072cb42ea
always0ne <si8363@naver.com>
2020-08-07 00:01:49 +0900

    refactor: auth refactoring
    ## 주요 변경점(클라이언트가 알아야 할 부분)
    - `authToken <Token>` -> ` Authorization Bearer <Token>`
    JWT 인증 표준이기 때문에 변경
    - accessToken 유효기간 10분
    - refreshToken 유효기간 일주일
    - 토큰이 만료되어 7번 FORBIDDEN error 발생시 `/auth/refresh` 로 refreshToken을 보내 accessToken 재발급
    - 토큰 발급 Header -> Body
    
    ## 구현 변경점
    - jjwt 최신버전으로 업데이트
    -  매 요청마다 DB check를 하지 않고, 독립적인 인증이 구현되어 UserDetail 걷어냄
    - JwtAuthenticationFilter에서 일부 Exception 핸들링
    컨트롤러레벨로 가기 전이기 때문에 직접 처리해야 하기 때문
    - jwtTokenProvicer 리팩토링
        - jjwt 버전업과 보안성 개선을 위해
        - refreshToken 발급을 위해 메소드를 쪼갬
    - secretkey 변경
    보안을 위한 업데이트 이후 특정 길이 이상이 되어야 한 규칙이 있어 수정
    
    ## 기타
    - 테스트 코드 이슈가 있어 우선은 한 테스트클래스에 모두 구현(merge 승인 받은 후 쪼개서 merge 할 예정)
    - Java 표준 주석 적용(이 부분에 대해서는 적용할지 의견을 듣고 싶어 작업을 함)
    - ApiDocs는 가독성 강화 이슈에 한번에 작성예정
    
    Resolves: #130

--- src/test/java/com/eskiiimo/web/security/controller/AuthControllerTest.java
+++ src/test/java/com/eskiiimo/web/security/controller/AuthControllerTest.java
@@ -1,24 +1,26 @@
 package com.eskiiimo.web.security.controller;
 
+import com.eskiiimo.repository.user.model.User;
+import com.eskiiimo.repository.user.repository.UserRepository;
 import com.eskiiimo.web.common.BaseControllerTest;
+import com.eskiiimo.web.security.request.RefreshRequest;
 import com.eskiiimo.web.security.request.SignInRequest;
 import com.eskiiimo.web.security.request.SignUpRequest;
-import com.eskiiimo.repository.user.repository.UserRepository;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.transaction.annotation.Transactional;
 
-import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
-import static org.springframework.restdocs.headers.HeaderDocumentation.responseHeaders;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
 import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @DisplayName("인증")
@@ -28,7 +30,7 @@ class AuthControllerTest extends BaseControllerTest {
 
     @Test
     @DisplayName("로그인")
-    void signinSuccess() throws Exception {
+    void signInSuccess() throws Exception {
 
         SignUpRequest signUpRequest = SignUpRequest.builder()
                 .userId("user1")
@@ -39,8 +41,8 @@ class AuthControllerTest extends BaseControllerTest {
         this.mockMvc.perform(post("/auth/signup")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(this.objectMapper.writeValueAsString(signUpRequest)));
-//
-                SignInRequest signInRequest = SignInRequest.builder()
+
+        SignInRequest signInRequest = SignInRequest.builder()
                 .userId("user1")
                 .password("testPassword")
                 .build();
@@ -49,15 +51,13 @@ class AuthControllerTest extends BaseControllerTest {
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(this.objectMapper.writeValueAsString(signInRequest)))
                 .andExpect(status().isOk())
-                .andExpect(header().exists("authToken"))
+                .andExpect(jsonPath("accessToken").exists())
+                .andExpect(jsonPath("refreshToken").exists())
                 .andDo(print())
                 .andDo(document("signin",
                         requestFields(
                                 fieldWithPath("userId").description("아이디"),
                                 fieldWithPath("password").description("비밀번호")
-                        ),
-                        responseHeaders(
-                                headerWithName("authToken").description("로그인 토큰")
                         )
                 ))
         ;
@@ -65,8 +65,8 @@ class AuthControllerTest extends BaseControllerTest {
     }
 
     @Test
-    @DisplayName("로그아웃")
-    void SignupSuccess() throws Exception {
+    @DisplayName("회원가입")
+    void SignUpSuccess() throws Exception {
         SignUpRequest signUpRequest = SignUpRequest.builder()
                 .userId("testid")
                 .userEmail("tester@eskiiimo.com")
@@ -86,7 +86,7 @@ class AuthControllerTest extends BaseControllerTest {
                                 fieldWithPath("name").description("이름"),
                                 fieldWithPath("userEmail").description("이메일주소")
                         )
-                        ))
+                ))
         ;
     }
 
@@ -96,8 +96,9 @@ class AuthControllerTest extends BaseControllerTest {
     void icCheckFailBecause_duplicatedID() throws Exception {
         testUserFactory.generateUser(1);
 
-        this.mockMvc.perform(post("/auth/idcheck/{checkId}","user1"))
+        this.mockMvc.perform(post("/auth/idcheck/{checkId}", "user1"))
                 .andExpect(status().isUnauthorized())
+                .andExpect(jsonPath("error").value(3))
                 .andDo(print())
                 .andDo(document("idCheck"))
         ;
@@ -109,10 +110,79 @@ class AuthControllerTest extends BaseControllerTest {
     void icCheckSuccess() throws Exception {
         testUserFactory.generateUser(1);
 
-        this.mockMvc.perform(post("/auth/idcheck/{checkId}","test"))
+        this.mockMvc.perform(post("/auth/idcheck/{checkId}", "test"))
                 .andExpect(status().isOk())
                 .andDo(print())
                 .andDo(document("idCheckOk"))
         ;
     }
+
+    @Test
+    @Transactional
+    @DisplayName("토큰 재발급 받기(성공)")
+    void refreshTokenSuccess() throws Exception {
+        User user = testUserFactory.generateUser(1);
+        RefreshRequest refreshRequest = RefreshRequest.builder()
+                .refreshToken(user.getRefreshToken())
+                .build();
+        this.mockMvc.perform(RestDocumentationRequestBuilders.post("/auth/refresh")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(this.objectMapper.writeValueAsString(refreshRequest)))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andDo(document("refresh"));
+    }
+
+    @Test
+    @Disabled       // Admin Api 개발 이후 활성화 필요
+    @Transactional
+    @DisplayName("토큰 재발급 받기(계정이 제제당했을 때)")
+    void refreshTokenFailBecauseDifferentUserToken() throws Exception {
+        User user = testUserFactory.generateUser(1);
+        // 계정 제제 로직 필요
+        RefreshRequest refreshRequest = RefreshRequest.builder()
+                .refreshToken(user.getRefreshToken())
+                .build();
+
+        this.mockMvc.perform(RestDocumentationRequestBuilders.post("/auth/refresh")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(this.objectMapper.writeValueAsString(refreshRequest)))
+                .andExpect(status().isForbidden())
+                .andDo(print());
+    }
+
+    @Test
+    @Transactional
+    @DisplayName("토큰 재발급 받기(서명이 일치하지 않았을 때)")
+    void refreshTokenFailBecauseSignature() throws Exception {
+        User user = testUserFactory.generateUser(1);
+        RefreshRequest refreshRequest = RefreshRequest.builder()
+                .refreshToken(user.getRefreshToken() + "e")
+                .build();
+
+        this.mockMvc.perform(RestDocumentationRequestBuilders.post("/auth/refresh")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(this.objectMapper.writeValueAsString(refreshRequest)))
+                .andExpect(status().isForbidden())
+                .andExpect(jsonPath("error").value(4))
+                .andDo(print());
+    }
+
+    @Test
+    @Transactional
+    @DisplayName("토큰 재발급 받기(토큰이 깨졌을 때 실패)")
+    void refreshTokenFailBecauseMalFormed() throws Exception {
+        User user = testUserFactory.generateUser(1);
+        RefreshRequest refreshRequest = RefreshRequest.builder()
+                .refreshToken("e" + user.getRefreshToken())
+                .build();
+
+        this.mockMvc.perform(RestDocumentationRequestBuilders.post("/auth/refresh")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(this.objectMapper.writeValueAsString(refreshRequest)))
+                .andExpect(status().isForbidden())
+                .andExpect(jsonPath("error").value(5))
+                .andDo(print());
+    }
+
 }
