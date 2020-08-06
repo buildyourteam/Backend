9bb946eb53ee6939f5f5372276ad61a1bae4c001
DESKTOP-김진영\rlawl <rlawlsdud419@naver.com>
2020-08-06 19:05:14 +0900

    refactor: ApiApplicationTest 활성화  ApiApplicationTest @Disabled 제거  Resolves:  See also: #73

--- src/test/java/com/eskiiimo/web/ApiApplicationTests.java
+++ src/test/java/com/eskiiimo/web/ApiApplicationTests.java
@@ -1,18 +1,15 @@
 package com.eskiiimo.web;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.ActiveProfiles;
 
-@ExtendWith(SpringExtension.class)
+
 @SpringBootTest
-@Disabled
-class ApiApplicationTests {
+@ActiveProfiles("test")
+public class ApiApplicationTests{
 
 	@Test
 	void contextLoads() {
 	}
-
 }
