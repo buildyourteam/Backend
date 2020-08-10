318e606fef6cd96a407da180224ccb978e3da094
김도영 <rlaehdud4992@ssu.ac.kr>
2020-08-10 16:15:21 +0900

    bug: 프로젝트 중복 지원 테스트 코드 작성
    [작성 내용]
    - 프로젝트 리더가 지원한 경우
    - 프로젝트 구성원이 지원한 경우
    - 프로젝트에 지원한 사용자가 중복 지원한 경우
    
    Resolves: #131

--- src/test/java/com/eskiiimo/web/projects/apply/ApplyProjectTest.java
+++ src/test/java/com/eskiiimo/web/projects/apply/ApplyProjectTest.java
@@ -3,6 +3,7 @@ package com.eskiiimo.web.projects.apply;
 import com.eskiiimo.repository.projects.model.Project;
 import com.eskiiimo.repository.user.model.User;
 import com.eskiiimo.web.common.BaseControllerTest;
+import com.eskiiimo.web.projects.enumtype.State;
 import com.eskiiimo.web.projects.request.ProjectApplyRequest;
 import com.eskiiimo.web.user.enumtype.UserActivate;
 import org.junit.jupiter.api.DisplayName;
@@ -62,6 +63,128 @@ public class ApplyProjectTest extends BaseControllerTest {
                 .andExpect(jsonPath("answers[2]").value("3번 응답"))
         ;
 
+    }
+
+    @Test
+    @DisplayName("프로젝트 지원_프로젝트 리더가 지원")
+    @WithMockUser(username = "user1")
+    void applyProjectFailBecause_DuplicateApplyWithLeader() throws Exception {
+
+        // Given
+        User user1 = testUserFactory.generateUser(1);
+        Project project = testProjectFactory.generateProject(1, user1, State.RECRUTING);
+        ProjectApplyRequest projectApplyRequest = testProjectFactory.generateProjectApplyRequest();
+
+        // When & Then
+        this.mockMvc.perform(RestDocumentationRequestBuilders.post("/projects/{projectId}/apply", project.getProjectId())
+                .content(objectMapper.writeValueAsString(projectApplyRequest))
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaTypes.HAL_JSON))
+                .andExpect(status().isForbidden())
+                .andExpect(jsonPath("error").value(108))
+                .andDo(print())
+                .andDo(document("applyProject",
+                        pathParameters(
+                                parameterWithName("projectId").description("프로젝트 아이디")
+                        ),
+                        requestFields(
+                                fieldWithPath("answers").description("지원서 응답"),
+                                fieldWithPath("role").description("지원할 역할"),
+                                fieldWithPath("introduction").description("자기소개")
+                        )
+                ))
+        ;
+    }
+
+    @Test
+    @DisplayName("프로젝트 지원_프로젝트 참여 인원이 중복 지원")
+    @WithMockUser(username = "user1")
+    void applyProjectFailBecause_DuplicateApplyWithProjectMember() throws Exception {
+
+        // Given
+        Project project = testProjectFactory.generateMyProject(0);
+        User user1 = testUserFactory.generateUser(1);
+        testProjectFactory.generateProjectMember(user1, project, true);
+        ProjectApplyRequest projectApplyRequest = testProjectFactory.generateProjectApplyRequest();
+
+        // When & Then
+        this.mockMvc.perform(RestDocumentationRequestBuilders.post("/projects/{projectId}/apply", project.getProjectId())
+                .content(objectMapper.writeValueAsString(projectApplyRequest))
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaTypes.HAL_JSON))
+                .andExpect(status().isForbidden())
+                .andExpect(jsonPath("error").value(108))
+                .andDo(print())
+                .andDo(document("applyProject",
+                        pathParameters(
+                                parameterWithName("projectId").description("프로젝트 아이디")
+                        ),
+                        requestFields(
+                                fieldWithPath("answers").description("지원서 응답"),
+                                fieldWithPath("role").description("지원할 역할"),
+                                fieldWithPath("introduction").description("자기소개")
+                        )
+                ))
+        ;
+
+    }
+
+    @Test
+    @DisplayName("프로젝트 지원_동일 사용자가 중복 지원")
+    @WithMockUser(username = "user1")
+    void applyProjectFailBecause_DuplicateApplyWithUser() throws Exception {
+
+        // Given
+        Project project = testProjectFactory.generateMyProject(0);
+        User me = testUserFactory.generateUser(1);
+        ProjectApplyRequest projectApplyRequest = testProjectFactory.generateProjectApplyRequest();
+
+        // When & Then
+        this.mockMvc.perform(RestDocumentationRequestBuilders.post("/projects/{projectId}/apply", project.getProjectId())
+                .content(objectMapper.writeValueAsString(projectApplyRequest))
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaTypes.HAL_JSON))
+                .andExpect(status().isCreated())
+                .andDo(print())
+                .andDo(document("applyProject",
+                        pathParameters(
+                                parameterWithName("projectId").description("프로젝트 아이디")
+                        ),
+                        requestFields(
+                                fieldWithPath("answers").description("지원서 응답"),
+                                fieldWithPath("role").description("지원할 역할"),
+                                fieldWithPath("introduction").description("자기소개")
+                        )
+                ))
+        ;
+        this.mockMvc.perform(get("/projects/{projectId}/apply/{userId}", project.getProjectId(), me.getUserId()))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("userName").value("user1"))
+                .andExpect(jsonPath("role").value("DEVELOPER"))
+                .andExpect(jsonPath("introduction").value("안녕하세요? 저는 그냥 개발자입니다."))
+                .andExpect(jsonPath("answers[0]").value("1번 응답"))
+                .andExpect(jsonPath("answers[1]").value("2번 응답"))
+                .andExpect(jsonPath("answers[2]").value("3번 응답"))
+        ;
+
+        this.mockMvc.perform(RestDocumentationRequestBuilders.post("/projects/{projectId}/apply", project.getProjectId())
+                .content(objectMapper.writeValueAsString(projectApplyRequest))
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaTypes.HAL_JSON))
+                .andExpect(status().isForbidden())
+                .andExpect(jsonPath("error").value(108))
+                .andDo(print())
+                .andDo(document("applyProject",
+                        pathParameters(
+                                parameterWithName("projectId").description("프로젝트 아이디")
+                        ),
+                        requestFields(
+                                fieldWithPath("answers").description("지원서 응답"),
+                                fieldWithPath("role").description("지원할 역할"),
+                                fieldWithPath("introduction").description("자기소개")
+                        )
+                ))
+        ;
     }
 
     @Test
