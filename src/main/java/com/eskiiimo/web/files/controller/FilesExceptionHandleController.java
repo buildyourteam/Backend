12265538b934b5c0a22451f7bfe7147e18d45178
김진영 <41534537+KimJinYounga@users.noreply.github.com>
2020-08-13 09:18:15 +0900

    refactor: ImageNotFoundException 처리 방식 변경 (#140)
    * refactor: ImageNotFoundException 처리 방식 변경
    
    이미지가 존재하지 않을 때 기본이미지를 반환해야 하므로 exception을 발생시키지 않고 서버에 저장되어 있는 default이미지를 반환한다.
    
    ## Exception
     - FilesExceptionHandleController에서 ProfileImageNotFoundException, ProjectImageNotFoundException 제거
    
    ## Service
     - ProfileImageService, ProjectImageService 다운로드 로직 변경
    
    ## Test
     - Default Image Test 추가
    
    Resolves: #120
    See also: -
    
    * refactor: default image 위치 환경파일에 설정
    
    ## application.yml
     - Project, Profile : 기본 이미지 위치 추가
    
    ## FileUploadProperties
     - Project, Profile: Object 형으로 변경
    
    Resolves: #120
    See also: -
    
    * refactor: files 하위 폴더에 default image 저장
    
    Resolves: #120
    See also: -
    
    * refactor: .idea 제거
    
    Resolves: #120
    See also: -

--- src/main/java/com/eskiiimo/web/files/controller/FilesExceptionHandleController.java
+++ src/main/java/com/eskiiimo/web/files/controller/FilesExceptionHandleController.java
@@ -3,8 +3,6 @@ package com.eskiiimo.web.files.controller;
 import com.eskiiimo.web.common.response.ErrorResponse;
 import com.eskiiimo.web.files.exception.FileDownloadException;
 import com.eskiiimo.web.files.exception.FileNameException;
-import com.eskiiimo.web.files.exception.ProfileImageNotFoundException;
-import com.eskiiimo.web.files.exception.ProjectImageNotFoundException;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
@@ -31,17 +29,4 @@ public class FilesExceptionHandleController {
         return new ErrorResponse("303", exception.getMessage());
     }
 
-    @ExceptionHandler(ProfileImageNotFoundException.class)
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    @ResponseBody
-    public ErrorResponse handleProfileImageNotFound(ProfileImageNotFoundException exception) {
-        return new ErrorResponse("305", exception.getMessage());
-    }
-
-    @ExceptionHandler(ProjectImageNotFoundException.class)
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    @ResponseBody
-    public ErrorResponse handleProjectImageNotFound(ProjectImageNotFoundException exception) {
-        return new ErrorResponse("306", exception.getMessage());
-    }
 }
